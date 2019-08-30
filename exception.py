ee177393f83a76447271f13c4b4e5c1849e49ba4
Tenyjose <tenyjose18@gmail.com>
2019-08-30 17:46:09 +0530

    lan python exception handling

---
+++ exception.py
@@ -0,0 +1,18 @@
+a=10
+b=5
+
+try:
+    print("RESOURCE OPENED")
+    c=a/b                                       #when error occures except block gets excecuted
+    print(c)
+    d=int(input("Enter a number :: "))
+    print(c/d)
+except ValueError:
+    print("CHECK OUT YOUR INPUT VALUE :) ")
+except ZeroDivisionError:
+    print("YOU CANT DIVIDE NUMBER WITH ZERO :)")
+except Exception:
+    print("OOPS SOMETHING WENT WRONG :( ")
+finally:                                            #excecuted each and every time
+    print("RESOURCE CLOSED")
+print("BYE:)")
